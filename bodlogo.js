ae2e906a6d855e8fb1e7da200955311850e62109
Otgonbyr <otgonbayar0503@gmail.com>
2023-10-10 09:11:48 +0800

    10

---
+++ bodlogo.js
@@ -0,0 +1,17 @@
+/*
+Math.sqrt(x2-x1)^2 + (y2-y1)^2
+x2 = 0
+x1 = 0
+y2 = 0
+y1 = 0
+*/
+let x2 = 5;
+let x1 = 4;
+let y2 = 5;
+let y1 = 4;
+
+let sum = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1) ) ;
+
+console.log(Math.sqrt(5 - 4) * (5 - 4) + (5 + 4) * (5 + 4))
+
+
---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+ax2 + bx + c = 0
+d = b^2 -4ac
+x1 = (-b + Math.sqrt(d))/ 2 * a
+x2 = (-b + Math.sqrt(d))/ 2 * a
+*/
+
+let a = 0;
+let b = 0;
+let c = 0;
+let sum
+
+console.log
+
+
+
