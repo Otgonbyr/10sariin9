1e63a32363e5ef4c1cf7d2ee98fc68215b26c7ef
Otgonbyr <otgonbayar0503@gmail.com>
2023-10-11 12:21:21 +0800

    loop

---
+++ loop.js
@@ -0,0 +1,43 @@
+/* 
+for( let a = 2; a <= 100; a++){
+    if (a === 2 || a === 3 || a === 5 || a === 7) {
+        console.log(a);
+    } else if (a % 2 === 0 || a % 3 === 0 || a % 5 === 0 || a % 7 === 0) {
+    } else {
+        console.log(a);
+    }
+}
+*/
+
+/* 
+let num = 45
+for(let a = 0; a < 100; i++){
+    if( a === num){
+        console.log('result:')
+        break
+    }
+    console.log(a)
+}
+*/
+
+/*
+let num = 0
+
+for(let a = 1; a <= 35; a++){
+    if(a % 2 === 0){
+        num = num + a
+    }
+}
+console.log(num)
+*/
+
+/*
+let num = 0
+
+for(let a = 1; a <= 35; a++){
+    if(a % 1 !== 0){
+        num = num + a
+    }
+}
+console.log(num)
+*/
