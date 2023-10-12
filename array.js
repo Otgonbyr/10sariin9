3d3bd784ae6ee2e1587e5d6dfb49ba321a870bc4
Otgonbyr <otgonbayar0503@gmail.com>
2023-10-12 12:28:30 +0800

    array

--- array.js
+++ array.js
@@ -6,11 +6,35 @@ for(let a = 0; a < num.length; a++){
 }
 */
 
+
+/*
 let reverseNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 let temp = reverseNum[reverseNum.length -1]
 
 for(let a = reverseNum.length -2; a >= reverseNum.length; a--){
     reverseNum[a + 1] = reverseNum[a]
 }
-reverseNum[0] = temp
+reverseNum[0] = temp;
 console.log(reverseNum);
+*/
+
+/**
+ let num =[4, 5, 1, 5, 6, 7, 2], target = 9
+
+for (let a = 0; a < num.length; a++){
+    for(let b = 0; b < num.length; b++)
+}
+ */
+
+
+let num = [1, 2, 3, 4, 5]
+let str = ""
+
+for(let a = 0; a < num.length; a++){
+    for(let b = 0 ; b < a + 1; b++){
+        str = str + " " +num[b]
+    }
+    console.log(str)
+    str = ""
+}
+
