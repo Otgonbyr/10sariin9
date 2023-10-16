039b58f5ac2ce08f44778dfbd1e4d90fe9f0358d
Otgonbyr <otgonbayar0503@gmail.com>
2023-10-16 12:32:59 +0800

    loop

--- twoSum.js
+++ twoSum.js
@@ -1,13 +1,38 @@
-let num = [2, 7, 11, 15, 4, 9]
-let target = 9;
+/* twoSum */
+// let num = [2, 7, 11, 15, 4, 9]
+// let target = 9;
 
-function NUMBER() {
-    for (let a = 0; a < num.length; a++) {
-        for (let b = a; b < num.length; b++) {
-            if (target === num[a] + num[b]) {
-                return [a, b]
-            }
-        }
-    }
-}
-console.log(NUMBER())
+// function NUMBER() {
+//     for (let a = 0; a < num.length; a++) {
+//         for (let b = a; b < num.length; b++) {
+//             if (target === num[a] + num[b]) {
+//                 return [a, b]
+//             }
+//         }
+//     }
+// }
+// console.log(NUMBER())
+
+
+/* threeSum */ 
+// let num = [2, 7, 11, 15, 4, 9, 3]
+// let target = 18;
+
+// function NUMBER() {
+//     for (let a = 0; a < num.length; a++) {
+//         for (let b = a; b < num.length; b++) {
+//             for (let c = b; c < num.length; c++) {
+//                  if (target === num[a] + num[b] + num[c]) {
+//                 return [a, b, c]
+//             }
+//             }
+//         }
+//     }
+// }
+// console.log(NUMBER())
+
+
+let nums = [1, 6, 2, 3, 7, 4, 9, 8, 5]
+    nums.sort()
+    console.log(nums)
+    
